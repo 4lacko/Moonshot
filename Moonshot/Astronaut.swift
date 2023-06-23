c46f6eaa82427c470b5d9047f1c93745bfead0e5
Flacko <f@ak68a.co>
2023-06-23 13:26:38 -0400

    first commit

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ayo Moreira on 6/21/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/MoonshotApp.swift
+++ Moonshot/MoonshotApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct MoonshotApp: App {
+struct MoonshotApp: App {    
     var body: some Scene {
         WindowGroup {
             ContentView()
